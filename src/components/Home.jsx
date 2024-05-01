d97c1b9d1292713e441bac85b94584c8842290d3
Omar Cruz <omar.cruzr97@hotmail.com>
2024-05-01 15:43:29 -0600

    updating

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,13 +9,13 @@ import 'firebase/compat/auth';
 import '../css/Home.css';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDMvttW6uWMalJiQLOlyJwaYdv9HNchHas",
+    authDomain: "squirrel-trello.firebaseapp.com",
+    projectId: "squirrel-trello",
+    storageBucket: "squirrel-trello.appspot.com",
+    messagingSenderId: "349870855963",
+    appId: "1:349870855963:web:95b9eab2f8c0628978e5be",
+    measurementId: "G-2NZBWTDLTQ"
 };
 
 const Home = () => {
